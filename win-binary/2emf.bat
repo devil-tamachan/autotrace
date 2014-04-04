cce47bd6e8f5afff58f1b2546a3b9ced9ef6aebf
devil-tamachan <devil.tamachan@gmail.com>
2014-04-04 20:33:13 +0900

    0.31.1.3 日本語ファイル名を通るようにバグ回避対応

--- win-binary/2emf.bat
+++ win-binary/2emf.bat
@@ -1,3 +1,6 @@
-cd /d %~dp0
-autotrace.exe --centerline --output-format=emf --output-file=%~n1.emf %1
+cd /d "%~dp0"
+copy /b /Y "%1" "%TEMP%\\t%~x1"
+autotrace.exe --centerline --output-format=emf --output-file="%TEMP%\\t2.emf" "%TEMP%\\t%~x1"
+del /Q "%TEMP%\\t%~x1"
+copy /b /Y "%TEMP%\\t2.emf" "%~d1%~p1%~n1.emf"
 pause
--- win-binary/2svg.bat
+++ win-binary/2svg.bat
@@ -1,3 +1,6 @@
-cd /d %~dp0
-autotrace.exe --centerline --output-format=svg --output-file=%~n1.svg %1
+cd /d "%~dp0"
+copy /b /Y "%1" "%TEMP%\\t%~x1"
+autotrace.exe --centerline --output-format=svg --output-file="%TEMP%\\t2.svg" "%TEMP%\\t%~x1"
+del /Q "%TEMP%\\t%~x1"
+copy /b /Y "%TEMP%\\t2.svg" "%~d1%~p1%~n1.svg"
 pause
