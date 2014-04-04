1d95e540d52177f06e685eccb542cb7dd3623a11
devil-tamachan <devil.tamachan@gmail.com>
2014-04-04 20:49:16 +0900

    0.31.1.4 引数にダブルクオートが含まれていると動作しないバグ修正

--- win-binary/2emf.bat
+++ win-binary/2emf.bat
@@ -1,5 +1,5 @@
 cd /d "%~dp0"
-copy /b /Y "%1" "%TEMP%\\t%~x1"
+copy /b /Y "%~1" "%TEMP%\\t%~x1"
 autotrace.exe --centerline --output-format=emf --output-file="%TEMP%\\t2.emf" "%TEMP%\\t%~x1"
 del /Q "%TEMP%\\t%~x1"
 copy /b /Y "%TEMP%\\t2.emf" "%~d1%~p1%~n1.emf"
